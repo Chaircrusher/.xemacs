856f722b6f6871c3e0e1b4cc4fd6e1359fb25cfc
Kent Williams <kwilliams@leepfrog.com>
2015-01-26 14:02:18 -0600

    COMP: remove icicles

--- custom.el
+++ custom.el
@@ -1,3 +1,19 @@
+;;; uncomment this line to disable loading of "default.el" at startup
+;; (setq inhibit-default-init t)
+
+;; enable visual feedback on selections
+;(setq transient-mark-mode t)
+
+;; default to better frame titles
+(setq frame-title-format
+      (concat  "%b - emacs@" (system-name)))
+
+;; default to unified diffs
+(setq diff-switches "-u")
+
+;; always end a file with a newline
+(setq require-final-newline 'query)
+
 (server-start)
 (setenv "EDITOR" "emacsclient")
 
